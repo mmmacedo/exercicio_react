17c1c60bcb2d48c2b571a3f8f54f3f2114663aa7
mmmacedo <murilommacedo666@gmail.com>
2019-03-29 09:10:12 -0300

    Componente que consome outro componente

---
+++ ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default propos => (
+    <div>
+        <Member name='John' lastName='Silva'/>
+        <Member name='Maria' lastName='Silva'/>
+        <Member name='José' lastName='Silva'/>
+        <Member name='Lucia' lastName='Silva'/>
+    </div>
+)
--- ex4/index.jsx
+++ ex4/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SilvaFamily from './silvaFamily'
+import {Primeiro, Segundo} from "./componente";
 
 ReactDOM.render(
-    <SilvaFamily/>
+    <div>
+        <Primeiro/>
+        <Segundo/>
+    </div>
     , document.getElementById('app'));
