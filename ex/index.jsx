2890c036f2679391c25b690d02116fd0ca01bbb3
mmmacedo <murilommacedo666@gmail.com>
2019-04-03 15:42:03 -0300

    Incremento com React + redux

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { combineReducers, createStore} from 'redux';
-import { Provider } from 'react-redux';
-import Field from './field';
-import FieldReducer from './fieldReducer';
+import {combineReducers, createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import counterReducer from './counterReducer';
+import Counter from './counter';
 
 const reducers = combineReducers({
-    field: FieldReducer //função reducer
+    counter: counterReducer
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
-        <Field initialValue='Teste'/>
+        <Counter/>
     </Provider>
-    , document.getElementById('app'));
+    , document.getElementById('app'))
